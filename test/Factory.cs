d5b0edf29de683c7285aed02308ad1506cb44d1d
mdhakin <mdhakin@alaska.edu>
2021-06-01 11:37:22 -0800

    factory update

--- test/Factory.cs
+++ test/Factory.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DataBinary;
+﻿using DataBinary;
 namespace test
 {
     public static class Factory
