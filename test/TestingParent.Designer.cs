451db0258a538d031f6fb502f6f7f805a4389d92
mdhakin <mdhakin@alaska.edu>
2021-08-19 10:15:04 -0800

    Add Progect Icon

--- test/TestingParent.Designer.cs
+++ test/TestingParent.Designer.cs
@@ -595,6 +595,7 @@ namespace test
             this.Controls.Add(this.statusStrip);
             this.Controls.Add(this.toolStrip);
             this.Controls.Add(this.menuStrip);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.IsMdiContainer = true;
             this.MainMenuStrip = this.menuStrip;
             this.Name = "TestingParent";
