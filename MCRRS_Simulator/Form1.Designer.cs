d3af356bad8fd0853d7d427be62e8247cc63f2dc
mdhakin <mdhakin@alaska.edu>
2021-09-01 17:26:18 -0800

    Added a test simulator app

---
+++ MCRRS_Simulator/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace MCRRS_Simulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::MCRRS_Simulator.Properties.Resources.FWD_INDICATOR;
+            this.pictureBox2.Location = new System.Drawing.Point(42, 95);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(49, 14);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MCRRS_Simulator.Properties.Resources.MCRRS_MAIN;
+            this.pictureBox1.Location = new System.Drawing.Point(346, 264);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(327, 133);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 300;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(679, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "HP water Pressue";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1080, 635);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MCRRS_Simulator/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MCRRS_Simulator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool Inforward;
+        UInt32 ct = 782;
+        string[] results;
+        //string[] hpwaterpressure;
+        //string[] hpwaterhydpressure;
+        Compare_dataset.Compare_dataset data;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            int[] test = new int[94];
+
+            for (int i = 0; i < test.Length; i++)
+            {
+                test[i] = i;
+            }
+            //int[] hpwaterpsi = new int[1];
+            //int[] hpwaterhydpsi = new int[1];
+
+            test[0] = 43;
+            //hpwaterpsi[0] = 6;
+            //hpwaterhydpsi[0] = 5;
+            data = new Compare_dataset.Compare_dataset(@"C:\output\5590\5590.dat", test);
+            results = data.getResult();
+            Inforward = false; // 42
+
+            //Compare_dataset.Compare_dataset hpwater1 = new Compare_dataset.Compare_dataset(@"C:\output\5590\5590.dat", hpwaterpsi);
+
+            //hpwaterpressure = hpwater1.getResult();
+            File.WriteAllLines(@"all.csv", results);
+            
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //string[] parts = results[ct].Split('|');
+            //label1.Text = DataBinary.UT.UnixTimeStampToDateTime(Convert.ToDouble(parts[0])).ToString();
+            //string[] hpwaterparts = hpwaterpressure[ct].Split('|');
+            //if(parts[1] == "1")
+            //{
+            //    pictureBox2.Visible = true;
+            //}else
+            //{
+            //    pictureBox2.Visible = false;
+            //}
+            //label2.Text = "HP water Pressue " + hpwaterparts[1] + " psi";
+            ct++;
+        }
+    }
+
+    
+
+}
